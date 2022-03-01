6b788fb878d655824690a382e547aebcc97701d2
Jonathan Aschmann <jonathan.aschmann@derivco.co.im>
2022-03-01 14:54:18 +0000

    added CLI menu and moved pyramid stuff to own class

--- Pyramid/Program.cs
+++ Pyramid/Program.cs
@@ -1,68 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Pyramid.Sequences;
 
 namespace Pyramid
 {
     internal class Program
     {
-        private const char EmptySpace = '*';
-
         static void Main(string[] args)
         {
-            Draw(CalculatePyramid(28, new AlphabetSequence()));
-            Console.WriteLine(string.Empty);
-            Draw(CalculatePyramid(12, new NumberSequence()));
-            Console.WriteLine(string.Empty);
-            DrawUpsideDown(CalculatePyramid(10, new SmileyFaceSequence()));
+            HandleUserInput();
+        }
+
+        private static void HandleUserInput()
+        {
+            Console.WriteLine("How high should the pyramid be?");
+            var heightInput = Console.ReadLine();
+
+            var height = 0;
+            if(!int.TryParse(heightInput, out height))
+            {
+                Console.WriteLine($"\r\n {heightInput} is not a valid number, try again..");
+                
+                HandleUserInput();
+            }
+
+            ISequence sequence = null;
+
+            var invalid = true;
+            while (invalid)
+            {
+                Console.WriteLine("\r\n\r\nWhich sequence should the pyramid use?");
+                Console.WriteLine("A for Alphabet");
+                Console.WriteLine("N for Number");
+                Console.WriteLine("S for Smiley Face");
+
+                var sequenceKey = Console.ReadKey();
+
+                switch (sequenceKey.Key)
+                {
+                    case ConsoleKey.A:
+                        sequence = new AlphabetSequence();
+                        invalid = false;
+                        break;
+                    case ConsoleKey.N:
+                        sequence = new NumberSequence();
+                        invalid = false;
+                        break;
+                    case ConsoleKey.S:
+                        sequence = new SmileyFaceSequence();
+                        invalid = false;
+                        break;
+                    default:
+                        Console.WriteLine($"\r\n\r\n{sequenceKey.Key} is an invalid choice! try again...");
+                        break;
+                }
+            }
+
+            invalid = true;
+            while (invalid)
+            {
+                Console.WriteLine("\r\n\r\n\r\nShould the pyramid be drawn upside down?");
+                Console.WriteLine("Y");
+                Console.WriteLine("N");
+                
+                var upsideDownKey = Console.ReadKey();
+
+                Console.WriteLine("\r\n\r\n");
+
+                switch (upsideDownKey.Key)
+                {
+                    case ConsoleKey.Y:
+                        Pyramid.DrawUpsideDown(Pyramid.CalculatePyramid(height, sequence));
+                        invalid = false;
+                        break;
+                    case ConsoleKey.N:
+                        Pyramid.Draw(Pyramid.CalculatePyramid(height, sequence));
+                        invalid = false;
+                        break;
+                    default:
+                        Console.WriteLine($"\r\n\r\n{upsideDownKey.Key} is an invalid choice! try again...");
+                        break;
+                }
+            }
+
+            invalid = true;
+            while (invalid)
+            {
+                Console.WriteLine("\r\n\r\nDraw another?");
+                Console.WriteLine("Y");
+                Console.WriteLine("N");
+
+                var drawAnotherKey = Console.ReadKey();
+
+                Console.WriteLine("\r\n\r\n");
+
+                switch (drawAnotherKey.Key)
+                {
+                    case ConsoleKey.Y:
+                        HandleUserInput();
+                        break;
+                    case ConsoleKey.N:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine($"\r\n\r\n{drawAnotherKey.Key} is an invalid choice! try again...");
+                        break;
+                }
+            }
 
             Console.Read();
-        }
-
-        private static IEnumerable<string> CalculatePyramid(int height, ISequence sequence)
-        {
-            var result = new List<string>();
-            var counter = 1;
-
-            for (var h = 1; h < height + 1; h++)
-            {
-                var row = String.Empty;
-                for (var x = 0; x < height - h; x++)
-                {
-                    row += EmptySpace.ToString();
-                }
-                for (var x = 0; x < counter; x++)
-                {
-                    row += sequence.GetCurrentCharacter();
-                }
-                for (var x = 0; x < height - h; x++)
-                {
-                    row += EmptySpace.ToString();
-                }
-
-                counter += 2;
-                sequence.IncrementSequenceCharacter();
-                result.Add(row);
-            }
-
-            return result;
-        }
-
-        private static void Draw(IEnumerable<string> input)
-        {
-            foreach (var row in input)
-            {
-                Console.WriteLine(row);
-            }
-        }
-
-        private static void DrawUpsideDown(IEnumerable<string> input)
-        {
-            foreach (var row in input.Reverse())
-            {
-                Console.WriteLine(row);
-            }
         }
     }
 }
---
+++ Pyramid/Pyramid.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Pyramid.Sequences;
+
+namespace Pyramid
+{
+    internal class Pyramid
+    {
+        private const char EmptySpace = '*';
+
+        public static IEnumerable<string> CalculatePyramid(int height, ISequence sequence)
+        {
+            var result = new List<string>();
+            var counter = 1;
+
+            for (var h = 1; h < height + 1; h++)
+            {
+                var row = String.Empty;
+                for (var x = 0; x < height - h; x++)
+                {
+                    row += EmptySpace.ToString();
+                }
+                for (var x = 0; x < counter; x++)
+                {
+                    row += sequence.GetCurrentCharacter();
+                }
+                for (var x = 0; x < height - h; x++)
+                {
+                    row += EmptySpace.ToString();
+                }
+
+                counter += 2;
+                sequence.IncrementSequenceCharacter();
+                result.Add(row);
+            }
+
+            return result;
+        }
+
+        public static void Draw(IEnumerable<string> input)
+        {
+            foreach (var row in input)
+            {
+                Console.WriteLine(row);
+            }
+        }
+
+        public static void DrawUpsideDown(IEnumerable<string> input)
+        {
+            foreach (var row in input.Reverse())
+            {
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
